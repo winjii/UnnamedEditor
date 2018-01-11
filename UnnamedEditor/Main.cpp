0390dc2e87db2908932db611859a4c856716402e
うぃんじー <wing3196@gmail.com>
2018-01-12 01:43:14 +0900

    バグを再現

--- UnnamedEditor/Main.cpp
+++ UnnamedEditor/Main.cpp
@@ -10,162 +10,178 @@
 using namespace FontDataPicker;
 
 namespace UnnamedEditor {
-	
-	void GsubReaderTest() {
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		FT_Face face;
-		FT_New_Face(lib, "C:/Windows/Fonts/msmincho.ttc", 0, &face);
 
-		FT_GlyphSlot slot = face->glyph;
-		Image image;
-		DynamicTexture texture;
-		auto init = [&](int pw, int ph) {
-			FT_Set_Pixel_Sizes(face, pw, ph);
+void GsubReaderTest() {
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	FT_Face face;
+	FT_New_Face(lib, "C:/Windows/Fonts/msmincho.ttc", 0, &face);
 
-			GsubReader gr(face);
+	FT_GlyphSlot slot = face->glyph;
+	Image image;
+	DynamicTexture texture;
+	auto init = [&](int pw, int ph) {
+		FT_Set_Pixel_Sizes(face, pw, ph);
 
-			unsigned int charCode = L'。';
-			FT_UInt gid = FT_Get_Char_Index(face, charCode);
-			FT_Load_Glyph(face, gr.vertSubstitute(gid), FT_LOAD_NO_BITMAP); //MS明朝において、NO_BITMAPにしないと読み込めないケースあり
-			FT_Render_Glyph(face->glyph, FT_Render_Mode::FT_RENDER_MODE_NORMAL);
-			FT_Bitmap bitmap = face->glyph->bitmap;
-			image = Image(bitmap.width, bitmap.rows);
-			for (int r = 0; r < bitmap.rows; r++) {
-				for (int c = 0; c < bitmap.width; c++) {
-					HSV gray(Color(bitmap.buffer[r*bitmap.width + c]));
-					image[r][c] = HSV(0, 0, 1 - gray.v);
-				}
-			}
-			texture = DynamicTexture(image);
+		GsubReader gr(face);
 
-		};
-		int pw = 16, ph = 16;
-		init(pw, ph);
-		while (System::Update()) {
-			if (KeyUp.down()) {
-				pw++; ph++;
-				init(pw, ph);
-			}
-			else if (KeyDown.down()) {
-				pw = Max(1, pw - 1); ph = Max(1, ph - 1);
-				init(pw, ph);
-			}
-			Rect(0, 0, Window::Size()).draw(Palette::White);
-			Point pos(100 - (slot->metrics.horiBearingX >> 6), 100 - (slot->metrics.horiBearingY >> 6));
-			//Rect(pos, bitmap.width, bitmap.rows).drawFrame(0, 5, Palette::Blue);
-			texture.fill(image);
-			texture.draw(pos);
-		}
-	}
-
-	void FontTest() {
-		using namespace UnnamedEditor::Font;
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		UnnamedEditor::Font::Font font(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
-		String s = L"「山村、（mucho）。」";
-		auto v = font.renderString(s.toUTF16());
-
-		Graphics::SetBackground(Palette::Gray);
-		while (System::Update()) {
-			Vec2 pen(100, 100);
-			for (int i = 0; i < v.size(); i++) {
-				pen = v[i]->draw(pen);
+		unsigned int charCode = L'。';
+		FT_UInt gid = FT_Get_Char_Index(face, charCode);
+		FT_Load_Glyph(face, gr.vertSubstitute(gid), FT_LOAD_NO_BITMAP); //MS明朝において、NO_BITMAPにしないと読み込めないケースあり
+		FT_Render_Glyph(face->glyph, FT_Render_Mode::FT_RENDER_MODE_NORMAL);
+		FT_Bitmap bitmap = face->glyph->bitmap;
+		image = Image(bitmap.width, bitmap.rows);
+		for (int r = 0; r < bitmap.rows; r++) {
+			for (int c = 0; c < bitmap.width; c++) {
+				HSV gray(Color(bitmap.buffer[r*bitmap.width + c]));
+				image[r][c] = HSV(0, 0, 1 - gray.v);
 			}
 		}
-	}
+		texture = DynamicTexture(image);
 
-	void FairCopyFieldTest() {
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		FairCopyField::FairCopyField fc(0, 0, Window::Width(), Window::Height(), lib, 20);
-		fc.setText(L"　吾輩は猫である。名前はまだない。　どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。");
-		while (System::Update()) {
-			fc.update();
+	};
+	int pw = 16, ph = 16;
+	init(pw, ph);
+	while (System::Update()) {
+		if (KeyUp.down()) {
+			pw++; ph++;
+			init(pw, ph);
+		}
+		else if (KeyDown.down()) {
+			pw = Max(1, pw - 1); ph = Max(1, ph - 1);
+			init(pw, ph);
+		}
+		Rect(0, 0, Window::Size()).draw(Palette::White);
+		Point pos(100 - (slot->metrics.horiBearingX >> 6), 100 - (slot->metrics.horiBearingY >> 6));
+		//Rect(pos, bitmap.width, bitmap.rows).drawFrame(0, 5, Palette::Blue);
+		texture.fill(image);
+		texture.draw(pos);
+	}
+}
+
+void FontTest() {
+	using namespace UnnamedEditor::Font;
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	UnnamedEditor::Font::Font font(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
+	String s = L"「山村、（mucho）。」";
+	auto v = font.renderString(s.toUTF16());
+
+	Graphics::SetBackground(Palette::Gray);
+	while (System::Update()) {
+		Vec2 pen(100, 100);
+		for (int i = 0; i < v.size(); i++) {
+			pen = v[i]->draw(pen);
 		}
 	}
+}
 
-	void WorkspaceTest() {
-		Window::Resize(Size(1280, 720));
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		Workspace::Workspace w(Vec2(0, 0), Vec2(Window::Width(), Window::Height()), lib);
-		while (System::Update()) {
-			w.update();
+void FairCopyFieldTest() {
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	FairCopyField::FairCopyField fc(0, 0, Window::Width(), Window::Height(), lib, 20);
+	fc.setText(L"　吾輩は猫である。名前はまだない。　どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。");
+	while (System::Update()) {
+		fc.update();
+	}
+}
+
+void WorkspaceTest() {
+	Window::Resize(Size(1280, 720));
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	Workspace::Workspace w(Vec2(0, 0), Vec2(Window::Width(), Window::Height()), lib);
+	while (System::Update()) {
+		w.update();
+	}
+}
+
+void GlyphTest() {
+	using namespace UnnamedEditor::Font;
+	using namespace UnnamedEditor;
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	UnnamedEditor::Font::Font fontV(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
+	UnnamedEditor::Font::Font fontH(lib, "C:/Windows/Fonts/msmincho.ttc", 30, false);
+	auto glyphsV = fontV.renderString(u"むーちょろくいちさん");
+	auto glyphsH = fontH.renderString(u"mucho613");
+	auto draw = [](const std::vector<SP<const UnnamedEditor::Font::Glyph>> &glyphs) {
+		Vec2 pen(100, 100);
+		for (int i = 0; i < (int)glyphs.size(); i++) {
+			glyphs[i]->boundingBox(pen, -Math::Pi/6.0).drawFrame(0.5, Palette::White);
+			pen = glyphs[i]->draw(pen, Palette::Red, -Math::Pi/6.0);
+		}
+	};
+	while (System::Update()) {
+		draw(glyphsV);
+		draw(glyphsH);
+	}
+}
+
+void DraftPaperTest() {
+	using namespace UnnamedEditor::Font;
+	using namespace UnnamedEditor;
+	using namespace UnnamedEditor::Workspace;
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	UnnamedEditor::Font::Font fontV(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
+	UnnamedEditor::Font::Font fontH(lib, "C:/Windows/Fonts/msmincho.ttc", 30, false);
+	auto glyphsV = fontV.renderString(u"むーーーーーーーーーーーーーーちょろくいちさん");
+	auto glyphsH = fontH.renderString(u"mucho613");
+	DraftPaper dpV(glyphsV, -Math::Pi/6.0);
+	DraftPaper dpH(glyphsH, Math::Pi/6.0);
+	dpV.setPos(Window::Size()/2.0);
+	dpH.setPos(Window::Size()/2.0);
+	Vec2 marginV = dpV.desirableMargin();
+	Vec2 marginH = dpH.desirableMargin();
+	RectF rectV(dpV.getPos() - marginV, marginV*2);
+	RectF rectH(dpH.getPos() - marginH, marginH*2);
+	Graphics::SetBackground(Palette::White);
+	while (System::Update()) {
+		dpV.draw();
+		rectV.drawFrame(1, Palette::Blue);
+		dpH.draw();
+		rectH.drawFrame(1, Palette::Blue);
+	}
+}
+
+void Glyph_scaleTest() {
+	FT_Library lib;
+	FT_Init_FreeType(&lib);
+	UnnamedEditor::Font::Font font0(lib, "C:/Windows/Fonts/msmincho.ttc", 32, true);
+	UnnamedEditor::Font::Font font1(lib, "C:/Windows/Fonts/msmincho.ttc", 31, true);
+	String str = L"三人寄ればソレイユ！"; //OpenSiv3Dのバグにより変な線が入る
+	//描画時に座標をasPoint()すると直るが...
+	auto g0 = font0.renderString(str.toUTF16());
+	auto g1 = font1.renderString(str.toUTF16());
+	bool mode = false;
+
+	Graphics::SetBackground(Palette::White);
+	while (System::Update()) {
+		if (MouseL.down()) mode = !mode;
+		auto &glyphs = mode ? g1 : g0;
+		Vec2 pen(200, 100);
+		for each (auto g in glyphs) {
+			pen = g->draw(pen, Palette::Black, 0, 0.9);
 		}
 	}
+}
 
-	void GlyphTest() {
-		using namespace UnnamedEditor::Font;
-		using namespace UnnamedEditor;
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		UnnamedEditor::Font::Font fontV(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
-		UnnamedEditor::Font::Font fontH(lib, "C:/Windows/Fonts/msmincho.ttc", 30, false);
-		auto glyphsV = fontV.renderString(u"むーちょろくいちさん");
-		auto glyphsH = fontH.renderString(u"mucho613");
-		auto draw = [](const std::vector<SP<const UnnamedEditor::Font::Glyph>> &glyphs) {
-			Vec2 pen(100, 100);
-			for (int i = 0; i < (int)glyphs.size(); i++) {
-				glyphs[i]->boundingBox(pen, -Math::Pi/6.0).drawFrame(0.5, Palette::White);
-				pen = glyphs[i]->draw(pen, Palette::Red, -Math::Pi/6.0);
-			}
-		};
-		while (System::Update()) {
-			draw(glyphsV);
-			draw(glyphsH);
+void OpenSiv3DBug() {
+	Image img(32, 32);
+	for (int i = 0; i < img.height(); i++) {
+		for (int j = 0; j < img.width(); j++) {
+			if (i > 5) img[i][j] = Color(Palette::White, 255);
+			else img[i][j] = Color(Palette::White, 0);
 		}
 	}
+	Texture texture(img);
 
-	void DraftPaperTest() {
-		using namespace UnnamedEditor::Font;
-		using namespace UnnamedEditor;
-		using namespace UnnamedEditor::Workspace;
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		UnnamedEditor::Font::Font fontV(lib, "C:/Windows/Fonts/msmincho.ttc", 30, true);
-		UnnamedEditor::Font::Font fontH(lib, "C:/Windows/Fonts/msmincho.ttc", 30, false);
-		auto glyphsV = fontV.renderString(u"むーーーーーーーーーーーーーーちょろくいちさん");
-		auto glyphsH = fontH.renderString(u"mucho613");
-		DraftPaper dpV(glyphsV, -Math::Pi/6.0);
-		DraftPaper dpH(glyphsH, Math::Pi/6.0);
-		dpV.setPos(Window::Size()/2.0);
-		dpH.setPos(Window::Size()/2.0);
-		Vec2 marginV = dpV.desirableMargin();
-		Vec2 marginH = dpH.desirableMargin();
-		RectF rectV(dpV.getPos() - marginV, marginV*2);
-		RectF rectH(dpH.getPos() - marginH, marginH*2);
-		Graphics::SetBackground(Palette::White);
-		while (System::Update()) {
-			dpV.draw();
-			rectV.drawFrame(1, Palette::Blue);
-			dpH.draw();
-			rectH.drawFrame(1, Palette::Blue);
-		}
+	Graphics::SetBackground(Palette::White);
+	while (System::Update()) {
+		texture.scale(0.9).draw(Vec2(187.40000000000001, 102.70000000000000), Palette::Black);
 	}
-
-	void Glyph_scaleTest() {
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		UnnamedEditor::Font::Font font0(lib, "C:/Windows/Fonts/msmincho.ttc", 32, true);
-		UnnamedEditor::Font::Font font1(lib, "C:/Windows/Fonts/msmincho.ttc", 31, true);
-		String str = L"一三三三三三人寄ればソレイユ！"; //三や一などの横棒が一番上にある字体において、描画が崩れる(なんか線が入る？)
-		//描画時に座標をasPoint()すると直るが...
-		auto g0 = font0.renderString(str.toUTF16());
-		auto g1 = font1.renderString(str.toUTF16());
-		bool mode = false;
-
-		Graphics::SetBackground(Palette::White);
-		while (System::Update()) {
-			if (MouseL.down()) mode = !mode;
-			auto &glyphs = mode ? g1 : g0;
-			Vec2 pen(200, 100);
-			for each (auto g in glyphs) {
-				pen = g->draw(pen, Palette::Black, 0, 0.9);
-			}
-		}
-	}
+}
 
 }
 
@@ -178,8 +194,9 @@ void Main()
 		WorkspaceTest,
 		GlyphTest,
 		DraftPaperTest,
-		Glyph_scaleTest
-	} runMode = RunMode::Glyph_scaleTest;
+		Glyph_scaleTest,
+		OpenSiv3DBug
+	} runMode = RunMode::OpenSiv3DBug;
 
 	if (runMode == RunMode::GsubReaderTest) {
 		UnnamedEditor::GsubReaderTest();
@@ -201,5 +218,8 @@ void Main()
 	}
 	else if (runMode == RunMode::Glyph_scaleTest) {
 		UnnamedEditor::Glyph_scaleTest();
+	}
+	else if (runMode == RunMode::OpenSiv3DBug) {
+		UnnamedEditor::OpenSiv3DBug();
 	}
 }
